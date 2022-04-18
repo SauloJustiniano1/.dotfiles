e2c89cb03307bb8a972b3595b0a3f61061843ed9
Saulo Justiniano <saulojustiniano1@gmail.com>
2022-04-18 02:16:20 -0300

    🚩Update: .dotfiles

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# .microfiles
-🔒⚙️Repositório de configurações geral do PC (Programação)
+<h1 align="center">🚀Developer<h1>
+
+<section align="center">
+  <img src=".github/preview.png">
+</section>
+
+##
+
+<p align="center"><strong>🔒⚙️Repositório de configurações gerais do computador!</strong></p>
--- powershell/user_profile.ps1
+++ powershell/user_profile.ps1
@@ -1,4 +1,4 @@
-# set PowerShell to UTF-8
+# Set PowerShell to UTF-8
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
 
 # Prompt
@@ -12,9 +12,12 @@ oh-my-posh --init --shell pwsh --config $omp_config | Invoke-Expression
 # Icons
 Import-Module -Name Terminal-Icons
 
-# Function Touch for powershell
+# Env
+$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
+
+# Function "Touch" for powershell
 function touch {
-  Param(
+  Param (
     [Parameter(Mandatory=$true)]
     [string]$Path
   )
@@ -26,27 +29,51 @@ function touch {
   }
 }
 
-# PSReadLine
-# Set-PSReadLineOption -EditMode Emacs
-# Set-PSReadLineOption -BellStyle None
-# Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
-# Set-PSReadLineOption -PredictionSource History
-
-# Env
-$env:GIT_SSH = "C:\Windows\system32\OpenSSH\ssh.exe"
-
-# Alias
-Set-Alias -Name vim -Value nvim
-Set-Alias ll ls
-Set-Alias c clear
-Set-Alias g git
-Set-Alias grep findstr
-Set-Alias np 'C:\Windows\notepad.exe'
-Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
-Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
-
 # Utilities
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
+
+function d {
+  cd 'D:\'
+}
+
+function c {
+  cd 'C:\Users\SauloJustiniano\'
+}
+
+function dev-p {
+  cd 'D:\development\projetos\'
+}
+
+function dev {
+  cd 'D:\development\'
+}
+
+function q {
+  cd ..
+}
+
+function mine {
+  explorer "C:\Users\SauloJustiniano\Appdata\Roaming\.minecraft"
+}
+
+# Alias
+Set-Alias -Name serv -Value Get-Service
+Set-Alias -Name vim -Value nvim
+Set-Alias -Name ll -Value ls
+Set-Alias -Name cls -Value clear
+Set-Alias -Name st -Value status
+Set-Alias -Name g -Value git
+Set-Alias -Name grep -Value findstr
+
+Set-Alias np "C:\Windows\notepad.exe"
+Set-Alias tig "C:\Program Files\Git\usr\bin\tig.exe"
+Set-Alias less "C:\Program Files\Git\usr\bin\less.exe"
+
+# PSReadLine
+# Set-PSReadLineOption -EditMode Emacs
+# Set-PSReadLineOption -BellStyle None
+# Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+# Set-PSReadLineOption -PredictionSource History
